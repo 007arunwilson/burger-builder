e46648099f7e30f0a07abbcf4f4f9d4b3f54bcc9
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-03 12:43:18 +0530

    Added basic reducer & action

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import actions from './actions';
+
+const intitialState = {
+    ingredients:null,
+    totalPrice:0,
+};
+
+const reducer = (state = intitialState,action) => {
+    return {...state};
+}
+
+export default reducer;
