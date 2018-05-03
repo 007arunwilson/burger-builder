0faab07514af2c75c61a041a9a6e8da9cff85d30
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-03 13:29:42 +0530

    Added ADD_INGREDIENT,REMOVE_INGREDIENT to reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,40 @@
-import actions from './actions';
+import * as actions from './actions';
 
 const intitialState = {
-    ingredients:null,
-    totalPrice:0,
+    ingredients: null,
+    totalPrice: 0,
 };
 
-const reducer = (state = intitialState,action) => {
-    return {...state};
+const reducer = (state = intitialState, action) => {
+
+    let updated_state = null;
+
+    switch (action.type) {
+        case actions.ADD_INGREDIENT:
+
+            updated_state = {
+                ...state,
+                ...state.ingredients
+            };
+
+            updated_state.ingredients[action.payload.ingredient] = updated_state.ingredients[action.payload.ingredient] + 1;
+
+            break;
+        case actions.REMOVE_INGREDIENT:
+
+            updated_state = {
+                ...state,
+                ...state.ingredients
+            };
+
+            updated_state.ingredients[action.payload.ingredient] = updated_state.ingredients[action.payload.ingredient] - 1;
+
+            break;
+
+        default:
+            return state;
+
+    }
 }
 
 export default reducer;
